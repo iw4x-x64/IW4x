8169fa49693d809c86ae30104735dc7412bd240b
William Roy <wroy@proton.me>
2026-01-27 20:45:53 -0500

    Patch s_cpuCount to use hardware concurrency

--- libiw4x/libiw4x/iw4x.cxx
+++ libiw4x/libiw4x/iw4x.cxx
@@ -126,6 +126,14 @@ namespace iw4x
         memwrite (0x1403B30DC, 0x00, 9);                                        // ^
         memwrite (0x1402864F0, "\xB0\x01\xC3");                                 // Patch Content_DoWeHaveContentPack to return true
 
+        // Patch s_cpuCount to use hardware concurrency in Sys_InitMainThread.
+        //
+        // Note that this may violate implicit engine assumptions about CPU
+        // topology. Current behavior is stable, but should be monitored for
+        // regressions.
+        //
+        *(uint32_t*) 0x14020DD06 = thread::hardware_concurrency ();
+
         // __scrt_common_main_seh
         //
         return reinterpret_cast<int (*) ()> (0x140358D48) ();
