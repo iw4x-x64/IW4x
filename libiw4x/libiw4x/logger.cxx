1fd6c151aa1bd94c2316168a45b55f232614dc0f
William Roy <wroy@proton.me>
2026-08-26 14:33:25 -0400

    Change log level display to compact form

--- libiw4x/libiw4x/logger.cxx
+++ libiw4x/libiw4x/logger.cxx
@@ -12,12 +12,39 @@ namespace iw4x::logger::detail
     quill::Logger*
     init ()
     {
-      quill::Backend::start ();
+      quill::BackendOptions o;
+      o.log_level_short_codes = {
+        "3", "2", "1", "D", "I", "N", "W", "E", "C", "B", "_"
+      };
 
-      auto cs (
-        quill::Frontend::create_or_get_sink <quill::ConsoleSink> ("console"));
+      quill::Backend::start (o);
+      quill::ConsoleSinkConfig cs;
+      quill::PatternFormatterOptions f (
+        "[%(log_level_short_code)] "
+        "%(short_source_location:<24) "
+        "%(message)");
 
-      return quill::Frontend::create_or_get_logger ("iw4x", move (cs));
+      // Normally Quill can decide whether to emit ANSI colour sequences
+      // by inspecting the terminal. This doesn't work reliably for us
+      // in some of the environments in which IW4x runs. One such case
+      // is Wine attached to a terminal that does understand escape
+      // sequences, where the terminal can still end up being detected
+      // as not supporting them.
+      //
+      // Since our console does support these sequences in the
+      // environments where we use it, force colour here instead of
+      // depending on that detection.
+      //
+      cs.set_colour_mode (quill::ConsoleSinkConfig::ColourMode::Always);
+
+      auto s (
+        quill::Frontend::create_or_get_sink <quill::ConsoleSink> ("console",
+                                                                  cs));
+
+      quill::Logger* l (
+        quill::Frontend::create_or_get_logger ("iw4x", move (s), f));
+
+      return l->set_log_level (quill::LogLevel::TraceL3), l;
     }
   }
 
